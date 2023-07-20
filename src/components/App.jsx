154d1a9a5387a6887c3a35d60d7e43f86ad2d8fb
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-20 16:34:48 +0300

    add application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+// import { useSelector } from 'react-redux';
+const App = () => {
+  return <div>rrrrrrr</div>;
 };
+export default App;
